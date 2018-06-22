543ab4e2f1f324427516b8694991afe5fc400ed4
Lucas Block <lucas.block@mulesoft.com>
2018-06-22 13:47:53 -0300

    add more paths

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -15,17 +15,18 @@ object App {
 
   val YAML: Boolean = false
   val AMF_PARSING: Boolean = false
-  val AMF_VALIDATION: Boolean = false
+  val AMF_VALIDATION: Boolean = true
   val AMF_DOUBLE_VALIDATION: Boolean = false
-  val AMF_RESOLUTION: Boolean = true
-  val RAML_PARSER: Boolean = true
+  val AMF_RESOLUTION: Boolean = false
+  val RAML_PARSER: Boolean = false
   val RAML_DOUBLE_PARSER: Boolean = false
   val SWAGGER_PARSER: Boolean = false
   val SWAGGER_VALIDATION: Boolean = false
 
 
-
-  val apiPath: String = "/Users/lucas.block/AWSDownloads/Unzipped/All/41a004ae-d95f-4c3b-9df9-94bf4102940c/de00bb74-26e9-4ed2-8a5a-c6310b138ebf/api.raml"
+//  val apiPath: String = "/Users/lucas.block/mulesoft/AMFScalaRunnerV2/run/testfile2.raml"
+  val apiPath: String = "/Users/lucas.block/AWSDownloads/Unzipped/All/3dab727d-b742-4b70-9893-9b8ab35a8ad1/bfbfdb27-830d-4806-bd2b-8fbbce991bd0/lob-api.raml"
+//  val apiPath: String = "/Users/lucas.block/mulesoft/current/runner/gard-el-gard-api-1.0.3-fat-raml/api.raml"
 
   val apiKind: APIType = RAML10
 //  val apiKind: APIType = RAML08
